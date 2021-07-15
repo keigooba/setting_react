99ad27acb16db0268783a07e00db44095d0ce292
keigooba <keigo2356@icloud.com>
2021-07-15 19:41:15 +0900

    git.sh実行

--- popular/src/App.tsx
+++ popular/src/App.tsx
@@ -1,6 +1,22 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./styles.css";
-// import axios from "axios";
+
+import { Login } from "./components/pages/Login";
+import { Page404 } from "./components/pages/Page404";
 
 export default function App() {
-  return <div className="App">test</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route path="*">
+            <Page404 />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
